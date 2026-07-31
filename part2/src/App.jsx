2bbd64c46eb3623bc6c2760ee81340f9b3a8427f
Raghav Srisankar <sriraghavsrisankar@gmail.com>
2026-07-31 12:11:35 -0700

    Complete Part 2 exercises 2.18 - 2.20

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,134 +1,35 @@
 import { useState, useEffect } from 'react'
-import personService from './services/persons'
-import { Filter, PersonForm, Persons } from './components/Phonebook'
-import Notification from './components/Notification'
+import axios from 'axios'
+import Countries from './components/Countries'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
   const [filterQuery, setFilterQuery] = useState('')
-  
-  // State for notifications (Exercise 2.16 & 2.17)
-  const [notificationMessage, setNotificationMessage] = useState(null)
-  const [isError, setIsError] = useState(false)
+  const [allCountries, setAllCountries] = useState([])
 
+  // Fetch all countries initially (Exercise 2.18)
   useEffect(() => {
-    personService
-      .getAll()
-      .then(initialPersons => {
-        setPersons(initialPersons)
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => {
+        setAllCountries(response.data)
       })
   }, [])
 
-  const handleNameChange = (event) => setNewName(event.target.value)
-  const handleNumberChange = (event) => setNewNumber(event.target.value)
   const handleFilterChange = (event) => setFilterQuery(event.target.value)
 
-  const showNotification = (message, error = false) => {
-    setNotificationMessage(message)
-    setIsError(error)
-    setTimeout(() => {
-      setNotificationMessage(null)
-    }, 5000)
-  }
-
-  // Exercise 2.16 & 2.17: Add or Update person
-  const addPerson = (event) => {
-    event.preventDefault()
-
-    const existingPerson = persons.find(
-      p => p.name.toLowerCase() === newName.trim().toLowerCase()
-    )
-
-    if (existingPerson) {
-      const confirmUpdate = window.confirm(
-        `${existingPerson.name} is already added to phonebook, replace the old number with a new one?`
+  const matchedCountries = filterQuery
+    ? allCountries.filter(country =>
+        country.name.common.toLowerCase().includes(filterQuery.toLowerCase())
       )
-
-      if (confirmUpdate) {
-        const updatedPerson = { ...existingPerson, number: newNumber }
-
-        personService
-          .update(existingPerson.id, updatedPerson)
-          .then(returnedPerson => {
-            setPersons(
-              persons.map(p => (p.id !== existingPerson.id ? p : returnedPerson))
-            )
-            setNewName('')
-            setNewNumber('')
-            showNotification(`Updated number for ${returnedPerson.name}`)
-          })
-          .catch(error => {
-            // Exercise 2.17*: Handle server removal error
-            showNotification(
-              `Information of ${existingPerson.name} has already been removed from server`,
-              true
-            )
-            setPersons(persons.filter(p => p.id !== existingPerson.id))
-          })
-      }
-      return
-    }
-
-    const personObject = {
-      name: newName,
-      number: newNumber
-    }
-
-    personService
-      .create(personObject)
-      .then(returnedPerson => {
-        setPersons(persons.concat(returnedPerson))
-        setNewName('')
-        setNewNumber('')
-        showNotification(`Added ${returnedPerson.name}`)
-      })
-  }
-
-  const handleDeleteOf = (id, name) => {
-    if (window.confirm(`Delete ${name}?`)) {
-      personService
-        .remove(id)
-        .then(() => {
-          setPersons(persons.filter(p => p.id !== id))
-          showNotification(`Deleted ${name}`)
-        })
-        .catch(error => {
-          showNotification(
-            `Information of ${name} has already been removed from server`,
-            true
-          )
-          setPersons(persons.filter(p => p.id !== id))
-        })
-    }
-  }
-
-  const personsToShow = persons.filter(person =>
-    person.name.toLowerCase().includes(filterQuery.toLowerCase())
-  )
+    : []
 
   return (
     <div>
-      <h2>Phonebook</h2>
-
-      <Notification message={notificationMessage} isError={isError} />
-
-      <Filter filterQuery={filterQuery} handleFilterChange={handleFilterChange} />
-
-      <h3>Add a new</h3>
-
-      <PersonForm
-        addPerson={addPerson}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
+      find countries <input value={filterQuery} onChange={handleFilterChange} />
+      <Countries
+        countries={matchedCountries}
+        setFilterQuery={setFilterQuery}
       />
-
-      <h3>Numbers</h3>
-
-      <Persons persons={personsToShow} handleDeleteOf={handleDeleteOf} />
     </div>
   )
 }
