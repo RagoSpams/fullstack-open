212d968aa23b2cc3a59689885951cbffe4b93ba9
Raghav Srisankar <sriraghavsrisankar@gmail.com>
2026-07-31 11:29:09 -0700

    Complete Part 2 exercises 2.1 - 2.5

---
+++ part2/src/components/Course.jsx
@@ -0,0 +1,36 @@
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  // Exercise 2.3*: Using reduce to calculate total exercises
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  )
+}
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
+
+export default Course
